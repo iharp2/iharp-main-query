9e6358fc4a5bdbe83214b61c5bb2afbfe1fa2b33
Yuchuan Huang <itisyuchuan@gmail.com>
2024-03-27 16:08:54 -0500

    init single agg test

--- src/query.py
+++ src/query.py
@@ -2,7 +2,7 @@
 Query Functions
 ===============
 
-All queries should return result as xarray.DataArray.
+All queries should return result as xarray.Dataset.
 """
 
 import xarray
@@ -17,13 +17,26 @@ def single_value_aggregation_query(
     start_datetime: str,
     end_datetime: str,
     aggregation_method: str,  # e.g., "mean", "max", "min"
-) -> xarray.DataArray:
+) -> xarray.Dataset:
     """
     "have a single value of a certain variable V over area A and time period T"
     E.g., "find the average temperature of Greenland over the last year."
     """
-    # TODO: Implement this function
-    return xarray.DataArray()
+    xa = xarray.open_dataset("data/ERA5/2m_temperature-2023-01-01.nc")  # TODO: Load the correct dataset
+    xa = xa.sel(
+        latitude=slice(max_lat, min_lat),
+        longitude=slice(min_lon, max_lon),
+        time=slice(start_datetime, end_datetime),
+    )
+    if aggregation_method == "mean":
+        result = xa.mean()
+    elif aggregation_method == "max":
+        result = xa.max()
+    elif aggregation_method == "min":
+        result = xa.min()
+    else:
+        raise ValueError(f"Invalid aggregation method: {aggregation_method}")
+    return result
 
 
 def time_series_query(
@@ -36,13 +49,13 @@ def time_series_query(
     end_datetime: str,
     time_resolution: str,
     aggregation_method: str,  # e.g., "mean", "max", "min"
-) -> xarray.DataArray:
+) -> xarray.Dataset:
     """
     "have a time series of a certain variable V over area A and time period T"
     E.g., "draw the average temperature heatmap of Greenland over the last year."
     """
     # TODO: Implement this function
-    return xarray.DataArray()
+    return xarray.Dataset()
 
 
 def heat_map_query(
@@ -54,13 +67,13 @@ def heat_map_query(
     start_datetime: str,
     end_datetime: str,
     aggregation_method: str,  # e.g., "mean", "max", "min"
-) -> xarray.DataArray:
+) -> xarray.Dataset:
     """
     "have the heatmap of a certain variable V over a certain area A and certain time period T"
     E.g., "draw the average temperature heatmap of Green- land over the last year."
     """
     # TODO: Implement this function
-    return xarray.DataArray()
+    return xarray.Dataset()
 
 
 def value_predicate_query(
@@ -73,13 +86,13 @@ def value_predicate_query(
     end_datetime: str,
     predicate_verb: str,  # e.g., "=", ">", "<", ">=", "<=", "!="
     predicate_value: float,
-) -> xarray.DataArray:
+) -> xarray.Dataset:
     """
     "retrieve records based on a certain variable criteria"
     E.g., "find all data records in the world over the last year where the temperature has exceeded 240."
     """
     # TODO: Implement this function
-    return xarray.DataArray()
+    return xarray.Dataset()
 
 
 def arbitrary_shape_query(
@@ -87,10 +100,10 @@ def arbitrary_shape_query(
     shape: str,  # e.g., "greenland", "iceland", "tri", "rec"
     start_datetime: str,
     end_datetime: str,
-) -> xarray.DataArray:
+) -> xarray.Dataset:
     """
     "retrieve records within an arbitrary shape"
     E.g., "find all data records in Greenland over the last year."
     """
     # TODO: Implement this function
-    return xarray.DataArray()
+    return xarray.Dataset()
--- test/query_test.py
+++ test/query_test.py
@@ -1,19 +1,27 @@
 import xarray
 from context import *
 
+MIN_LAT = 60
+MAX_LAT = 80
+MIN_LON = 285
+MAX_LON = 350
+START_DATETIME = "2023-01-01 00:00:00"
+END_DATETIME = "2023-01-01 09:00:00"
 
-def test_time_series_query():
-    result = time_series_query(
+
+def test_single_value_aggregation_query():
+    result = single_value_aggregation_query(
         variable="2m_temperature",
-        start_datetime="2023-01-01 00:00:00",
-        end_datetime="2023-01-01 10:00:00",
-        min_lat=50,
-        max_lat=55,
-        min_lon=10,
-        max_lon=15,
-        time_resolution="hour",
+        min_lat=MIN_LAT,
+        max_lat=MAX_LAT,
+        min_lon=MIN_LON,
+        max_lon=MAX_LON,
+        start_datetime=START_DATETIME,
+        end_datetime=END_DATETIME,
         aggregation_method="mean",
     )
-    assert isinstance(result, xarray.DataArray)
-    # assert result.shape == (0, 0, 0)
-    # assert result.values == xarray.DataArray().values
+    print(result.info())
+    assert isinstance(result, xarray.Dataset)  # check on type
+    assert result.sizes == xarray.core.utils.Frozen({})  # check on size
+    assert result.t2m.values[()] == 251.92674  # check on value
+
